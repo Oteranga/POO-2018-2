8d150559c934c667042d3fbd0ea02faf90e4f97d
Oteranga <40015080+Oteranga@users.noreply.github.com>
2018-08-28 10:52:08 -0500

    Create Segundo ejercicio.cpp

---
+++ Segundo ejercicio.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  
+using namespace std;
+int main() {
+    int numero1=0;
+    int numero2=0;
+    cout<<"Escribe un número: ";
+    cin>>numero1;
+    cout<<"Escribe un segundo numero: ";
+    cin>>numero2;
+    cout<<numero1/numero2;
+    cout<<(numero1*1.0)/numero2;
+return 0;
+}
